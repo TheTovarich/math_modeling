ed1a4ee24bdc2fd9c5679db6691a53af18741f4f
Андрей <80597030+TheTovarich@users.noreply.github.com>
2021-10-15 16:41:00 +0000

    и тут офигел просто

---
+++ lec_2_bool.py
@@ -0,0 +1,16 @@
+print(bool(2))#True
+
+print(bool('Good'))#True
+
+print(bool([1, 4, 5]))#True
+
+print(bool(0))#False Все числа кроме 0 True
+
+print(bool(''))#False
+
+print(bool([]))#False
+
+print(bool([[]]))#True Типо список это элемент
+
+print(bool(3>2))#True
+
---
+++ lec_2_log_oper.py
@@ -0,0 +1,13 @@
+a = 3
+b = 4
+c = 5
+
+if a > 4 and b == 2: #and Операция лог. И
+   print('Good')
+elif b > 3 or c==5: #or Операция лог. ИЛИ
+  print('Best') 
+else:
+  print('Bad') # !=  Это знак неравно
+
+
+
