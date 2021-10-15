c3e4fdc9836d9227153209ddae965c80611c35cf
Андрей <80597030+TheTovarich@users.noreply.github.com>
2021-10-15 15:54:59 +0000

    Мама я програмист!!!!!!!!!!!!(первая лаба)

---
+++ lab_1_task_4.py
@@ -0,0 +1,17 @@
+a = [1, 5, 'Good', 'Bad']
+b = [9, 'Blue', 'Red', 11]
+
+c = a[1] + b[3]
+print(c)
+
+d = a[2] + b[2]
+print(d)
+
+e = a[0] + b[0]
+print(e)
+
+f = a[1] ** b[3]
+print(f)
+
+g = a + b
+print(g)
