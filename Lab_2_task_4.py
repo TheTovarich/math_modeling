35c9d7648b299e9d422f872611c64bd7f0fa4333
Андрей <80597030+TheTovarich@users.noreply.github.com>
2021-11-12 10:42:30 +0000

    Сделал 4 и почти сделал 5

--- Lab_2_task_4.py
+++ Lab_2_task_4.py
@@ -1,2 +1,23 @@
+n = int(input('Введите кол-во членов'))
+n1 = n-2
+
+print(1)
+
+for i in range(0, n1 + 1):
+  if i == 0:
+    print(1, ' ')
+    b = 1
+  if i == 1:
+    print(2, ' ')
+    c = 2
+  if i != 0 and i != 1:
+    a = b + c
+    print(a, ' ')
+    b = c
+    c = a
+
+
+
+
 
 
